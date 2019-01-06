f0ee3dc0174c206dc436d577542da023994e5bc5
estherhu2012@gmail.com <estherhu2012@gmail.com>
2019-01-06 09:43:27 -0600

    Implement tf.matmul #94

--- src/TensorFlowNET.Core/Operations/gen_math_ops.cs
+++ src/TensorFlowNET.Core/Operations/gen_math_ops.cs
@@ -30,5 +30,18 @@ namespace Tensorflow
 
             return new Tensor(_op, 0, _op.OutputType(0));
         }
+
+        public static Tensor mat_mul(Tensor a, Tensor b, bool transpose_a = false, bool transpose_b = false)
+        {
+            var keywords = new Dictionary<string, object>();
+            keywords.Add("a", a);
+            keywords.Add("b", b);
+            keywords.Add("transpose_a", transpose_a);
+            keywords.Add("transpose_b", transpose_b);
+
+            var _op = _op_def_lib._apply_op_helper("MatMul", name: "MatMul", keywords: keywords);
+
+            return new Tensor(_op, 0, _op.OutputType(0));
+        }
     }
 }
