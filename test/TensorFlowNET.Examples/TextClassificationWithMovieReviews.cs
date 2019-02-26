244698887902ed1f755e60f64004986890cb378d
haiping008 <haiping008@gmail.com>
2019-02-26 17:24:43 -0600

    text classification sample 1.

--- test/TensorFlowNET.Examples/TextClassificationWithMovieReviews.cs
+++ test/TensorFlowNET.Examples/TextClassificationWithMovieReviews.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Text;
 using Tensorflow;
 using NumSharp.Core;
+using Newtonsoft.Json;
+using System.Linq;
+using Keras;
 
 namespace TensorFlowNET.Examples
 {
@@ -14,10 +17,20 @@ namespace TensorFlowNET.Examples
 
         public void Run()
         {
-            PrepareData();
+            var((train_data, train_labels), (test_data, test_labels)) = PrepareData();
+
+            Console.WriteLine($"Training entries: {train_data.size}, labels: {train_labels.size}");
+
+            // A dictionary mapping words to an integer index
+            var word_index = GetWordIndex();
+
+            train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                value: word_index["<PAD>"],
+                padding: "post",
+                maxlen: 256);
         }
 
-        private void PrepareData()
+        private ((NDArray, NDArray), (NDArray, NDArray)) PrepareData()
         {
             Directory.CreateDirectory(dir);
 
@@ -32,14 +45,39 @@ namespace TensorFlowNET.Examples
             NDArray x_train = File.ReadAllLines(Path.Join(dir, "x_train.txt"));
             NDArray labels_train = File.ReadAllLines(Path.Join(dir, "y_train.txt"));
             NDArray indices_train = File.ReadAllLines(Path.Join(dir, "indices_train.txt"));
-            x_train = x_train[indices_train];
-            labels_train = labels_train[indices_train];
+            // x_train = x_train[indices_train];
+            // labels_train = labels_train[indices_train];
 
             NDArray x_test = File.ReadAllLines(Path.Join(dir, "x_test.txt"));
             NDArray labels_test = File.ReadAllLines(Path.Join(dir, "y_test.txt"));
             NDArray indices_test = File.ReadAllLines(Path.Join(dir, "indices_test.txt"));
-            x_test = x_test[indices_test];
-            labels_test = labels_test[indices_test];
+            // x_test = x_test[indices_test];
+            // labels_test = labels_test[indices_test];
+
+            // not completed
+            var xs = x_train.hstack(x_test);
+            var labels = labels_train.hstack(labels_test);
+
+            var idx = x_train.size;
+            var y_train = labels_train;
+            var y_test = labels_test;
+
+            return ((x_train, y_train), (x_test, y_test));
+        }
+
+        private Dictionary<string, int> GetWordIndex()
+        {
+            var result = new Dictionary<string, int>();
+            var json = File.ReadAllText(Path.Join(dir, "imdb_word_index.json"));
+            var dict = JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
+
+            dict.Keys.Select(k => result[k] = dict[k] + 3).ToList();
+            result["<PAD>"] = 0;
+            result["<START>"] = 1;
+            result["<UNK>"] = 2; // unknown
+            result["<UNUSED>"] = 3;
+
+            return result;
         }
     }
 }
