31db0f78f7dc91736f493a829bfe8289ed61e44d
Bolo <36342729+PppBr@users.noreply.github.com>
2019-03-02 22:15:38 -0600

    implement tf.distribution and keep developing on NaiveBayes Classifier

---
+++ src/TensorFlowNET.Core/Operations/Distributions/normal.py.cs
@@ -0,0 +1,11 @@
+namespace Tensorflow
+{
+    class Normal : Distribution
+    {
+        public Normal (Tensor loc, Tensor scale, bool validate_args=false, bool allow_nan_stats=true, string name="Normal")
+        {
+            
+        }
+
+    }
+}
