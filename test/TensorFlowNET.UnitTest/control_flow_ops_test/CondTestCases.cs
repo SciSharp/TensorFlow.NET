33c4a9a45aec9c100725d137d4b22ac69a6c299f
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-04-13 20:15:32 +0200

    Operation: _update_inputs works now

--- test/TensorFlowNET.UnitTest/control_flow_ops_test/CondTestCases.cs
+++ test/TensorFlowNET.UnitTest/control_flow_ops_test/CondTestCases.cs
@@ -19,12 +19,12 @@ namespace TensorFlowNET.UnitTest.control_flow_ops_test
                 var y = tf.constant(5);
                 var z = control_flow_ops.cond(tf.less(x, y), () => tf.multiply(x, tf.constant(17)),
                     () => tf.add(y, tf.constant(23)));
-                //tf.train.export_meta_graph(@"D:\dev\tensorboard\logdir\sharp.meta", as_text: false);
+                tf.train.export_meta_graph(@"D:\dev\tensorboard\logdir\sharp.meta", as_text: false);
                 self.assertEquals(eval_scalar(z), 34);
             });
         }
 
-        [Ignore("This Test Fails due to missing edges in the graph!")]
+        //[Ignore("This Test Fails due to missing edges in the graph!")]
         [TestMethod]
         public void testCondFalse()
         {
