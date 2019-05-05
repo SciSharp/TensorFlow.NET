503b7816cf91ef7b7fb2e21b185f430bc49df8d1
Oceania2018 <haiping008@gmail.com>
2019-05-05 00:18:15 -0500

    Init bi-LSTM + CRF NER

---
+++ test/TensorFlowNET.Examples/Text/NER/BiLstmCrfNer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tensorflow;
+
+namespace TensorFlowNET.Examples
+{
+    public class BiLstmCrfNer : Python, IExample
+    {
+        public int Priority => 13;
+
+        public bool Enabled { get; set; } = true;
+        public bool ImportGraph { get; set; } = false;
+
+        public string Name => "bi-LSTM + CRF NER";
+
+        public void PrepareData()
+        {
+            
+        }
+
+        public bool Run()
+        {
+            return true;
+        }
+    }
+}
