43d7833b8d747b1ae1578dea4f27c5c0e8c8ce22
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-07-11 21:53:24 +0200

    added a benchmark suite

---
+++ src/TensorFlowNet.Benchmarks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace TensorFlowNet.Benchmark
+{
+    class Program
+    {
+        /// <summary>
+        /// dotnet NumSharp.Benchmark.dll (Benchmark Class Name)
+        /// dotnet NumSharp.Benchmark.dll nparange
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+#if DEBUG
+            IConfig config = new DebugInProcessConfig();
+#else
+            IConfig config = null;
+#endif
+
+            if (args?.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string name = $"TensorFlowNet.Benchmark.{args[i]}";
+                    var type = Type.GetType(name);
+                    BenchmarkRunner.Run(type, config);
+                }
+            }
+            else
+            {
+                BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, config);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
