49cfe84dba807e0b008e27b6b309c975d7e594eb
Antonio Cifonelli <antonio.cifonelli@gmail.com>
2019-07-19 17:17:41 +0200

    Add missing `operator <=`s
    Also unit testing all the operator cases.

--- src/TensorFlowNET.Core/Tensors/Tensor.Operators.cs
+++ src/TensorFlowNET.Core/Tensors/Tensor.Operators.cs
@@ -95,7 +95,6 @@ namespace Tensorflow
         public static Tensor operator <(int x, Tensor y) => gen_math_ops.less(x, y);
         public static Tensor operator <(Tensor x, Tensor y) => gen_math_ops.less(x, y);
         public static Tensor operator <(Tensor x, int y) => gen_math_ops.less(x, y);
-        public static Tensor operator <=(Tensor x, Tensor y) => gen_math_ops.less_equal(x, y);
         public static Tensor operator <(Tensor x, float y) => gen_math_ops.less(x, y);
         public static Tensor operator <(Tensor x, double y) => gen_math_ops.less(x, y);
 
@@ -106,6 +105,14 @@ namespace Tensorflow
         public static Tensor operator >=(Tensor x, int y) => gen_math_ops.greater_equal(x, y);
         public static Tensor operator >=(Tensor x, float y) => gen_math_ops.greater_equal(x, y);
         public static Tensor operator >=(Tensor x, double y) => gen_math_ops.greater_equal(x, y);
+
+        public static Tensor operator <=(int x, Tensor y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(float x, Tensor y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(double x, Tensor y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(Tensor x, Tensor y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(Tensor x, int y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(Tensor x, float y) => gen_math_ops.less_equal(x, y);
+        public static Tensor operator <=(Tensor x, double y) => gen_math_ops.less_equal(x, y);
 
         private static Tensor BinaryOpWrapper<Tx, Ty>(string name, Tx x, Ty y)
         {
