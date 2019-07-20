05c01d0b97755d5be951aceae912b4b02fadf334
Kerry Jiang <kerry-jiang@hotmail.com>
2019-07-20 08:42:18 -0700

    base classes for Tensorflow.Hub

---
+++ src/TensorFlowHub/MnistDataSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp;
+using Tensorflow;
+
+namespace Tensorflow.Hub
+{
+    public class MnistDataSet : DataSetBase
+    {
+        public int NumOfExamples { get; private set; }
+        public int EpochsCompleted { get; private set; }
+        public int IndexInEpoch { get; private set; }
+
+        public MnistDataSet(NDArray images, NDArray labels, TF_DataType dtype, bool reshape)
+        {
+            EpochsCompleted = 0;
+            IndexInEpoch = 0;
+
+            NumOfExamples = images.shape[0];
+
+            images = images.reshape(images.shape[0], images.shape[1] * images.shape[2]);
+            images.astype(dtype.as_numpy_datatype());
+            images = np.multiply(images, 1.0f / 255.0f);
+            Data = images;
+
+            labels.astype(dtype.as_numpy_datatype());
+            Labels = labels;
+        }
+    }
+}
---
+++ src/TensorFlowHub/MnistModelLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp;
+
+namespace Tensorflow.Hub
+{
+    public class MnistModelLoader : IModelLoader<MnistDataSet>
+    {
+        public Task<Datasets<MnistDataSet>> LoadAsync(ModelLoadSetting setting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
