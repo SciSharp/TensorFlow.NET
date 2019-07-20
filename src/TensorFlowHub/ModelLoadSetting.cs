ce94a5c56c8f898612ff65a7959ab3d43bc4f4fc
Kerry Jiang <kerry-jiang@hotmail.com>
2019-07-20 08:42:18 -0700

    base classes for Tensorflow.Hub

---
+++ src/TensorFlowHub/ModelLoadSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp;
+
+namespace Tensorflow.Hub
+{
+    public class ModelLoadSetting
+    {
+        public string TrainDir { get; set; }
+        public bool OneHot { get; set; }
+        public TF_DataType DtType { get; set; } = TF_DataType.TF_FLOAT;
+        public bool ReShape { get; set; }
+        public int ValidationSize { get; set; } = 5000;
+        public int? TrainSize  { get; set; }
+        public int? TestSize { get; set; }
+        public string SourceUrl { get; set; }
+    }
+}
