bcb0504705efd4a0c6f4b3c9de1f6dea6102f097
Kerry Jiang <kerry-jiang@hotmail.com>
2019-07-21 13:06:42 -0700

    added utils class for the Hub

---
+++ src/TensorFlowHub/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NumSharp;
+
+namespace Tensorflow.Hub
+{
+    public static class Utils
+    {
+        public static async Task DownloadAsync<TDataSet>(this IModelLoader<TDataSet> modelLoader, string url, string saveTo)
+            where TDataSet : IDataSet
+        {
+            var dir = Path.GetDirectoryName(saveTo);
+            var fileName = Path.GetFileName(saveTo);
+            await modelLoader.DownloadAsync(url, dir, fileName);
+        }
+
+        public static async Task DownloadAsync<TDataSet>(this IModelLoader<TDataSet> modelLoader, string url, string dirSaveTo, string fileName)
+            where TDataSet : IDataSet
+        {
+            if (!Path.IsPathRooted(dirSaveTo))
+                dirSaveTo = Path.Combine(AppContext.BaseDirectory, dirSaveTo);
+
+            if (!Directory.Exists(dirSaveTo))
+                Directory.CreateDirectory(dirSaveTo);
+            
+            using (var wc = new WebClient())
+            {
+                await wc.DownloadFileTaskAsync(url, Path.Combine(dirSaveTo, fileName));
+            }
+        }
+    }
+}
