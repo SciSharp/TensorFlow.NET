51b6c07f81ece5ed619affe4e3722632c55685ce
Oceania2018 <haiping008@gmail.com>
2019-07-25 07:26:19 -0500

    Init TensorFlow.Text project. put all UnitTest and Example into one project.

---
+++ test/TensorFlowNET.UnitTest/KerasTests.cs
@@ -0,0 +1,26 @@
+﻿using Tensorflow;
+using Keras.Layers;
+using NumSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TensorFlowNET.UnitTest
+{
+    [TestClass]
+    public class BaseTests
+    {
+        [TestMethod]
+        public void Dense_Tensor_ShapeTest()
+        {
+            var dense_1 = new Dense(1, name: "dense_1", activation: tf.nn.relu());
+            var input = new Tensor(np.array(new int[] { 3 }));
+            dense_1.__build__(input.TensorShape);
+            var outputShape = dense_1.output_shape(input.TensorShape);
+            var a = (int[])(outputShape.Dimensions);
+            var b = (int[])(new int[] { 1 });
+            var _a = np.array(a);
+            var _b = np.array(b);
+
+            Assert.IsTrue(np.array_equal(_a, _b));
+        }
+    }
+}
