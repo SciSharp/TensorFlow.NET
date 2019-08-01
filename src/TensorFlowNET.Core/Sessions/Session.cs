81c3da3399f8716fae33d9fa11c6add63da4f3ee
Oceania2018 <haiping008@gmail.com>
2019-07-31 23:50:57 -0500

    Fixed Session.LoadFromSavedModel(), Found accuracy regression for Logistic Regression.

--- src/TensorFlowNET.Core/Sessions/Session.cs
+++ src/TensorFlowNET.Core/Sessions/Session.cs
@@ -50,9 +50,16 @@ namespace Tensorflow
             var graph = c_api.TF_NewGraph();
             var status = new Status();
             var opt = c_api.TF_NewSessionOptions();
-
+            var tags = new string[] { "serve" };
             var buffer = new TF_Buffer();
-            var sess = c_api.TF_LoadSessionFromSavedModel(opt, IntPtr.Zero, path, new string[0], 0, graph, ref buffer, status);
+            var sess = c_api.TF_LoadSessionFromSavedModel(opt,
+                IntPtr.Zero,
+                path,
+                tags,
+                tags.Length,
+                graph,
+                ref buffer,
+                status);
 
             //var bytes = new Buffer(buffer.data).Data;
             //var meta_graph = MetaGraphDef.Parser.ParseFrom(bytes);
