cebc8b4bf58b386c5affee1dafe7db31a4b952cd
Eli Belash <elibelash@gmail.com>
2019-09-08 15:21:57 +0300

    Binding: Added None

--- src/TensorFlowNET.Core/Binding.cs
+++ src/TensorFlowNET.Core/Binding.cs
@@ -7,5 +7,10 @@ namespace Tensorflow
     public static partial class Binding
     {
         public static tensorflow tf { get; } = New<tensorflow>();
+
+        /// <summary>
+        ///     Alias to null, similar to python's None.
+        /// </summary>
+        public static readonly object None = null;
     }
 }
