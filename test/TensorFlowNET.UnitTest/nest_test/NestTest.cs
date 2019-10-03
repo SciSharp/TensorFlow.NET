698e628768e35f8bdaef70c1a797fcb6ed1314d6
Oceania2018 <haiping008@gmail.com>
2019-10-03 14:00:47 -0500

    rename projects TensorFlowHub and Datasets.

--- test/TensorFlowNET.UnitTest/nest_test/NestTest.cs
+++ test/TensorFlowNET.UnitTest/nest_test/NestTest.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using Colorful;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
 using NumSharp;
