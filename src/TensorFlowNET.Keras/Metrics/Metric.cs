4deb320885afedd3aa859b4c2ea1f51941245b39
Deepak Kumar <deepakkumar1984@gmail.com>
2020-01-12 02:06:43 +1030

    Sequential, Metric base and  Metric Util class skeleton methods added

--- src/TensorFlowNET.Keras/Metrics/Metric.cs
+++ src/TensorFlowNET.Keras/Metrics/Metric.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Tensorflow.Keras.Metrics
 {
-    public abstract class Metric
+    public abstract class Metric : Layers.Layer
     {
+        public string dtype
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Metric(string name, string dtype)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void __new__ (Metric cls, Args args, KwArgs kwargs) => throw new NotImplementedException();
+
+        public Tensor __call__(Metric cls, Args args, KwArgs kwargs) => throw new NotImplementedException();
+
+        public virtual Hashtable get_config() => throw new NotImplementedException();
+
+        public virtual void reset_states() => throw new NotImplementedException();
+
+        public abstract void update_state(Args args, KwArgs kwargs);
+
+        public abstract Tensor result();
+
+        public void add_weight(string name, TensorShape shape= null, VariableAggregation aggregation= VariableAggregation.Sum,
+                                VariableSynchronization synchronization = VariableSynchronization.OnRead, Initializers.Initializer initializer= null, 
+                                string dtype= null) => throw new NotImplementedException();
     }
 }
--- src/TensorFlowNET.Keras/Utils/MetricsUtils.cs
+++ src/TensorFlowNET.Keras/Utils/MetricsUtils.cs
@@ -1,10 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Tensorflow.Keras.Utils
 {
-    class MetricsUtils
+    public class MetricsUtils
     {
+        public static class Reduction
+        {
+            public const string SUM = "sum";
+            public const string SUM_OVER_BATCH_SIZE = "sum_over_batch_size";
+            public const string WEIGHTED_MEAN = "weighted_mean";
+        }
+
+        public static class ConfusionMatrix
+        {
+            public const string TRUE_POSITIVES = "tp";
+            public const string FALSE_POSITIVES = "fp";
+            public const string TRUE_NEGATIVES = "tn";
+            public const string FALSE_NEGATIVES = "fn";
+        }
+
+        public static class AUCCurve
+        {
+            public const string ROC = "ROC";
+            public const string PR = "PR";
+
+            public static string from_str(string key) => throw new NotImplementedException();
+        }
+
+        public static class AUCSummationMethod
+        {
+            public const string INTERPOLATION = "interpolation";
+            public const string MAJORING = "majoring";
+            public const string MINORING = "minoring";
+
+            public static string from_str(string key) => throw new NotImplementedException();
+        }
+
+        public static dynamic update_state_wrapper(Func<Args, KwArgs, Func<bool>> update_state_fn) => throw new NotImplementedException();
+
+        public static dynamic result_wrapper(Func<Args, Tensor> result_fn) => throw new NotImplementedException();
+
+        public static WeakReference weakmethod(MethodInfo method) => throw new NotImplementedException();
+
+        public static void assert_thresholds_range(float[] thresholds) => throw new NotImplementedException();
+
+        public static void parse_init_thresholds(float[] thresholds, float default_threshold = 0.5f) => throw new NotImplementedException();
+
+        public static Operation update_confusion_matrix_variables(variables variables_to_update, Tensor y_true, Tensor y_pred, float[] thresholds,
+                                                    int? top_k= null,int? class_id= null, Tensor sample_weight= null, bool multi_label= false,
+                                                    Tensor label_weights= null) => throw new NotImplementedException();
+
+        private static Tensor _filter_top_k(Tensor x, int k) => throw new NotImplementedException();
+
+        private static (Tensor[], Tensor) ragged_assert_compatible_and_get_flat_values(Tensor[] values, Tensor mask = null) => throw new NotImplementedException();
     }
 }
