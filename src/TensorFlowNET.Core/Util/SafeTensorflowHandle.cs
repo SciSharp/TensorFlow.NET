79f3d7617fa1d4bc4b4b864330f184f93511c7f2
Sam Harwell <sam@tunnelvisionlabs.com>
2020-02-04 18:06:29 -0800

    Implement SafeTensorflowHandle with extensions

---
+++ src/TensorFlowNET.Core/Util/SafeTensorflowHandle.cs
@@ -0,0 +1,43 @@
+﻿/*****************************************************************************
+   Copyright 2018 The TensorFlow.NET Authors. All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+******************************************************************************/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Tensorflow.Util
+{
+    public abstract class SafeTensorflowHandle : SafeHandle
+    {
+        private protected SafeTensorflowHandle()
+            : base(IntPtr.Zero, ownsHandle: true)
+        {
+        }
+
+        private protected SafeTensorflowHandle(IntPtr handle)
+            : base(IntPtr.Zero, ownsHandle: true)
+        {
+            SetHandle(handle);
+        }
+
+        private protected SafeTensorflowHandle(IntPtr handle, bool ownsHandle)
+            : base(IntPtr.Zero, ownsHandle)
+        {
+            SetHandle(handle);
+        }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+    }
+}
