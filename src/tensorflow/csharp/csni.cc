8a2782c7f0352d961787087deb45841dd6e377f4
Oceania2018 <haiping008@gmail.com>
2020-05-16 15:15:04 -0500

    0.20.0-alpha

--- src/tensorflow/csharp/csni.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "tensorflow/c/c_api.h"
-
-int main() {
-  printf("Hello from TensorFlow C library version %s", TF_Version());
-  return 0;
-}
--- src/tensorflow/csharp/eager/cswrap_tfe.h
+++
@@ -1,4 +0,0 @@
-// Record the gradient for a given op.
-extern void TFE_Py_RecordGradient(const char* op_name, void* inputs,
-                                void* attrs, void* results,
-                                const char* name);
