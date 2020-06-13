d6e04005c9b9b6d1fec7d975008b3372766e13f5
Oceania2018 <haiping008@gmail.com>
2020-06-13 08:18:42 -0500

    0.20.x-preview1, add tf.keras namespace.

---
+++ src/TensorFlowNET.Core/Data/TensorSliceDataset.cs
@@ -0,0 +1,20 @@
+﻿using NumSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensorflow
+{
+    public class TensorSliceDataset
+    {
+        public TensorSliceDataset(params NDArray[] elements)
+        {
+
+        }
+
+        public TensorSliceDataset from_tensor_slices(params NDArray[] elements)
+        {
+            throw new NotImplementedException("");
+        }
+    }
+}
