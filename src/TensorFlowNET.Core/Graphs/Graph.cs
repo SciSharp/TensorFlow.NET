9f99b44f649981ed4961364e89fd38be18b03796
carb0n <58676303+carb0n@users.noreply.github.com>
2020-08-04 13:27:47 -0400

    fix build error

--- src/TensorFlowNET.Core/Graphs/Graph.cs
+++ src/TensorFlowNET.Core/Graphs/Graph.cs
@@ -17,6 +17,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Runtime.InteropServices;
 using static Tensorflow.Binding;
