5e0e8b1b955c0636df8872dd3d54cf0fb13fa013
Oceania2018 <haiping008@gmail.com>
2020-08-22 14:25:50 -0500

    fix iteration times issue.

--- test/TensorFlowNET.UnitTest/Dataset/DatasetTest.cs
+++ test/TensorFlowNET.UnitTest/Dataset/DatasetTest.cs
@@ -45,5 +45,21 @@ namespace TensorFlowNET.UnitTest.Dataset
                 value += 2;
             }
         }
+
+        [TestMethod]
+        public void FromTensorSlices()
+        {
+            var X = tf.constant(new[] { 2013, 2014, 2015, 2016, 2017 });
+            var Y = tf.constant(new[] { 12000, 14000, 15000, 16500, 17500 });
+
+            var dataset = tf.data.Dataset.from_tensor_slices(X, Y);
+            int n = 0;
+            foreach (var (item_x, item_y) in dataset)
+            {
+                print($"x:{item_x.numpy()},y:{item_y.numpy()}");
+                n += 1;
+            }
+            Assert.AreEqual(5, n);
+        }
     }
 }
