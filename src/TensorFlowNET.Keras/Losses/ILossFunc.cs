b73a6fea32d02b7c53cfc5330bafa30cb2b7eaaa
dataangel <dataangel@live.com>
2020-12-14 21:59:58 +0800

    Update ILossFunc.cs

--- src/TensorFlowNET.Keras/Losses/ILossFunc.cs
+++ src/TensorFlowNET.Keras/Losses/ILossFunc.cs
@@ -1,8 +1,8 @@
-﻿namespace Tensorflow.Keras.Losses
+namespace Tensorflow.Keras.Losses
 {
     public interface ILossFunc
     {
         string Reduction { get; }
-        Tensor Call(Tensor y_true, Tensor y_pred);
+        Tensor Call(Tensor y_true, Tensor y_pred, Tensor sample_weight = null);
     }
 }
