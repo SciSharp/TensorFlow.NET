d89609a4e3e998666b0aa5790a01b55bdc43978e
MPnoy <maxhb@yandex.ru>
2021-02-01 03:58:59 +0300

    Blank SimpleRNN and test for it

--- src/TensorFlowNET.Core/Keras/ArgsDefinition/RNNArgs.cs
+++ src/TensorFlowNET.Core/Keras/ArgsDefinition/RNNArgs.cs
@@ -1,6 +1,21 @@
-﻿namespace Tensorflow.Keras.ArgsDefinition
+﻿using System.Collections.Generic;
+
+namespace Tensorflow.Keras.ArgsDefinition
 {
     public class RNNArgs : LayerArgs
     {
+        public interface IRnnArgCell : ILayer
+        {
+            object state_size { get; }
+        }
+
+        public IRnnArgCell Cell { get; set; } = null;
+        public bool ReturnSequences { get; set; } = false;
+        public bool ReturnState { get; set; } = false;
+        public bool GoBackwards { get; set; } = false;
+        public bool Stateful { get; set; } = false;
+        public bool Unroll { get; set; } = false;
+        public bool TimeMajor { get; set; } = false;
+        public Dictionary<string, object> Kwargs { get; set; } = null;
     }
 }
