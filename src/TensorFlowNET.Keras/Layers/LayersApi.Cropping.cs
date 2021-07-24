73f710259ad7f98c4a612bdc45e37330a9cecd49
Kevin <kevinofcathay@gmail.com>
2021-07-24 12:19:43 +0800

    Added Cropping and Permute (+test)

---
+++ src/TensorFlowNET.Keras/Layers/LayersApi.Cropping.cs
@@ -0,0 +1,36 @@
+﻿using Tensorflow.NumPy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tensorflow.Keras.ArgsDefinition;
+
+namespace Tensorflow.Keras.Layers {
+      public partial class LayersApi {
+            /// <summary>
+            /// Cropping layer for 1D input
+            /// </summary>
+            /// <param name="cropping">cropping size</param>
+            public Cropping1D Cropping1D ( NDArray cropping )
+                => new Cropping1D(new CroppingArgs {
+                      cropping = cropping
+                });
+
+            /// <summary>
+            /// Cropping layer for 2D input <br/>
+            /// </summary>
+            public Cropping2D Cropping2D ( NDArray cropping, Cropping2DArgs.DataFormat data_format = Cropping2DArgs.DataFormat.channels_last )
+                => new Cropping2D(new Cropping2DArgs {
+                      cropping = cropping,
+                      data_format = data_format
+                });
+
+            /// <summary>
+            /// Cropping layer for 3D input <br/>
+            /// </summary>
+            public Cropping3D Cropping3D ( NDArray cropping, Cropping3DArgs.DataFormat data_format = Cropping3DArgs.DataFormat.channels_last )
+                => new Cropping3D(new Cropping3DArgs {
+                      cropping = cropping,
+                      data_format = data_format
+                });
+      }
+}
--- test/TensorFlowNET.Keras.UnitTest/Layers/Layers.Reshaping.Test.cs
+++ test/TensorFlowNET.Keras.UnitTest/Layers/Layers.Reshaping.Test.cs
@@ -4,37 +4,40 @@ using Tensorflow.NumPy;
 using static Tensorflow.Binding;
 using static Tensorflow.KerasApi;
 
-namespace TensorFlowNET.Keras.UnitTest
-{
-    [TestClass]
-    public class LayersReshapingTest : EagerModeTestBase
-    {
-        [TestMethod]
-        public void ZeroPadding2D()
-        {
-            Shape input_shape = (1, 1, 2, 2);
-            var x = np.arange(input_shape.size).reshape(input_shape);
-            var zero_padding_2d = keras.layers.ZeroPadding2D(new[,] { { 1, 0 }, { 1, 0 } });
-            var y = zero_padding_2d.Apply(x);
-            Assert.AreEqual((1, 2, 3, 2), y.shape);
-        }
+namespace TensorFlowNET.Keras.UnitTest {
+      [TestClass]
+      public class LayersReshapingTest : EagerModeTestBase {
+            [TestMethod]
+            public void ZeroPadding2D () {
+                  Shape input_shape = (1, 1, 2, 2);
+                  var x = np.arange(input_shape.size).reshape(input_shape);
+                  var zero_padding_2d = keras.layers.ZeroPadding2D(new[,] { { 1, 0 }, { 1, 0 } });
+                  var y = zero_padding_2d.Apply(x);
+                  Assert.AreEqual((1, 2, 3, 2), y.shape);
+            }
 
-        [TestMethod]
-        public void UpSampling2D()
-        {
-            Shape input_shape = (2, 2, 1, 3);
-            var x = np.arange(input_shape.size).reshape(input_shape);
-            var y = keras.layers.UpSampling2D(size: (1, 2)).Apply(x);
-            Assert.AreEqual((2, 2, 2, 3), y.shape);
-        }
+            [TestMethod]
+            public void UpSampling2D () {
+                  Shape input_shape = (2, 2, 1, 3);
+                  var x = np.arange(input_shape.size).reshape(input_shape);
+                  var y = keras.layers.UpSampling2D(size: (1, 2)).Apply(x);
+                  Assert.AreEqual((2, 2, 2, 3), y.shape);
+            }
 
-        [TestMethod]
-        public void Reshape()
-        {
-            var inputs = tf.zeros((10, 5, 20));
-            var outputs = keras.layers.LeakyReLU().Apply(inputs);
-            outputs = keras.layers.Reshape((20, 5)).Apply(outputs);
-            Assert.AreEqual((10, 20, 5), outputs.shape);
-        }
-    }
+            [TestMethod]
+            public void Reshape () {
+                  var inputs = tf.zeros((10, 5, 20));
+                  var outputs = keras.layers.LeakyReLU().Apply(inputs);
+                  outputs = keras.layers.Reshape((20, 5)).Apply(outputs);
+                  Assert.AreEqual((10, 20, 5), outputs.shape);
+            }
+
+            [TestMethod]
+            public void Permute () {
+                  var inputs = tf.zeros((2, 3, 4, 5));
+                  var outputs = keras.layers.Permute(new int[] { 3, 2, 1 }).Apply(inputs);
+                  Assert.AreEqual((2, 5, 4, 3), outputs.shape);
+            }
+
+      }
 }
