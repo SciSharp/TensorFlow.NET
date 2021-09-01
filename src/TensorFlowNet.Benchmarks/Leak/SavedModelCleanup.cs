c88b390a037dcb9c52d12c243b424c83d671bb57
Luke Bollam <lukeb@alertedh.com>
2021-09-01 11:32:59 +0800

    adding saved model cleanup benchmark

---
+++ src/TensorFlowNet.Benchmarks/Leak/SavedModelCleanup.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tensorflow.Benchmark.Leak
+{
+	
+	public class SavedModelCleanup
+	{
+		[Benchmark]
+		public void Run()
+		{
+			var modelDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var ClassifierModelPath = Path.Combine(modelDir, "Leak", "TestModel", "saved_model");
+
+			for (var i = 0; i < 1000; i++)
+			{
+				var session = Session.LoadFromSavedModel(ClassifierModelPath);
+
+				session.graph.Exit();
+				session.graph.Dispose();
+				session.Dispose();
+			}
+		}
+	}
+}
