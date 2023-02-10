5821275145e5123d1acbc4094acd2baef06ac138
Superpiffer <federico.giuliani86@gmail.com>
2023-02-10 16:21:26 +0100

    Reimplemented NDArray == and != operators, handling null values. Added unit tests.

---
+++ test/TensorFlowNET.UnitTest/NumPy/OperatorsTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tensorflow.NumPy;
+
+namespace TensorFlowNET.UnitTest.NumPy
+{
+    [TestClass]
+    public class OperatorsTest
+    {
+        [TestMethod]
+        public void EqualToOperator()
+        {
+            NDArray n1 = null;
+            NDArray n2 = new NDArray(1);
+
+            Assert.IsTrue(n1 == null);
+            Assert.IsFalse(n2 == null);
+            Assert.IsFalse(n1 == 1);
+            Assert.IsTrue(n2 == 1);
+        }
+
+        [TestMethod]
+        public void NotEqualToOperator()
+        {
+            NDArray n1 = null;
+            NDArray n2 = new NDArray(1);
+
+            Assert.IsFalse(n1 != null);
+            Assert.IsTrue(n2 != null);
+            Assert.IsTrue(n1 != 1);
+            Assert.IsFalse(n2 != 1);
+        }
+    }
+}
