016294d33fe7a8600561fa14e659f7651bbf7fde
Haiping Chen <haiping008@gmail.com>
2023-03-03 10:35:58 -0600

    Seperate SafeCheckpointReaderHandle.

---
+++ src/TensorFlowNET.Core/Checkpoint/SafeCheckpointReaderHandle.cs
@@ -0,0 +1,21 @@
+﻿using Tensorflow.Util;
+
+namespace Tensorflow.Checkpoint;
+
+public sealed class SafeCheckpointReaderHandle : SafeTensorflowHandle
+{
+    private SafeCheckpointReaderHandle() : base ()
+    {
+    }
+
+    public SafeCheckpointReaderHandle(IntPtr handle) : base(handle)
+    {
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        c_api.TF_DeleteCheckpointReader(handle);
+        SetHandle(IntPtr.Zero);
+        return true;
+    }
+}
