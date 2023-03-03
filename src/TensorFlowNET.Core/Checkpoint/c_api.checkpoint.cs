3dafbf04e90b54b418e831389d6233319d80dd45
Yaohui Liu <AsakusaRinne@gmail.com>
2023-03-03 11:22:31 +0800

    Fix the stucking of training when loading model.

--- src/TensorFlowNET.Core/Checkpoint/c_api.checkpoint.cs
+++ src/TensorFlowNET.Core/Checkpoint/c_api.checkpoint.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+using Tensorflow.Checkpoint;
 
 namespace Tensorflow
 {
     public unsafe partial class c_api
     {
         [DllImport(TensorFlowLibName)]
-        internal static extern IntPtr TF_NewCheckpointReader(string filename, SafeStatusHandle status);
+        internal static extern SafeCheckpointReaderHandle TF_NewCheckpointReader(string filename, SafeStatusHandle status);
         [DllImport(TensorFlowLibName)]
         internal static extern void TF_DeleteCheckpointReader(IntPtr reader);
         [DllImport(TensorFlowLibName)]
-        internal static extern int TF_CheckpointReaderHasTensor(IntPtr reader, string name);
+        internal static extern int TF_CheckpointReaderHasTensor(SafeCheckpointReaderHandle reader, string name);
         [DllImport(TensorFlowLibName)]
-        internal static extern string TF_CheckpointReaderGetVariable(IntPtr reader, int index);
+        internal static extern IntPtr TF_CheckpointReaderGetVariable(SafeCheckpointReaderHandle reader, int index);
         [DllImport(TensorFlowLibName)]
-        internal static extern int TF_CheckpointReaderSize(IntPtr reader);
+        internal static extern int TF_CheckpointReaderSize(SafeCheckpointReaderHandle reader);
         [DllImport(TensorFlowLibName)]
-        internal static extern TF_DataType TF_CheckpointReaderGetVariableDataType(IntPtr reader, string name);
+        internal static extern TF_DataType TF_CheckpointReaderGetVariableDataType(SafeCheckpointReaderHandle reader, string name);
         [DllImport(TensorFlowLibName)]
-        internal static extern void TF_CheckpointReaderGetVariableShape(IntPtr reader, string name, long[] dims, int num_dims, SafeStatusHandle status);
+        internal static extern void TF_CheckpointReaderGetVariableShape(SafeCheckpointReaderHandle reader, string name, long[] dims, int num_dims, SafeStatusHandle status);
         [DllImport(TensorFlowLibName)]
-        internal static extern int TF_CheckpointReaderGetVariableNumDims(IntPtr reader, string name);
+        internal static extern int TF_CheckpointReaderGetVariableNumDims(SafeCheckpointReaderHandle reader, string name);
         [DllImport(TensorFlowLibName)]
-        internal static extern SafeTensorHandle TF_CheckpointReaderGetTensor(IntPtr reader, string name, SafeStatusHandle status);
+        internal static extern SafeTensorHandle TF_CheckpointReaderGetTensor(SafeCheckpointReaderHandle reader, string name, SafeStatusHandle status);
     }
 }
