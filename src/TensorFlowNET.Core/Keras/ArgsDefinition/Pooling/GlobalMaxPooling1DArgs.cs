f5eb4ff0a0950fa1b0c3af9b67950e4f4dc90a1a
Wanglongzhi2001 <583087864@qq.com>
2023-08-26 10:35:45 +0800

    fix: partially fix the bug of load_model

---
+++ src/TensorFlowNET.Core/Keras/ArgsDefinition/Pooling/GlobalMaxPooling1DArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensorflow.Keras.ArgsDefinition
+{
+    public class GlobalMaxPooling1DArgs : Pooling1DArgs
+    {
+    }
+}
