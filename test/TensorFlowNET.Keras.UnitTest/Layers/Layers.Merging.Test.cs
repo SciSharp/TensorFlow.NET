9f0ffa4bc83b181ddd525cf1b90d77a32e073fa3
Jucko13 <Jucko13@users.noreply.github.com>
2023-10-10 17:02:22 +0200

    Implemented unittests for Concatenate layers and calls
    The loading and saving of a simple model with a Concatenate layer is tested to check if the model is the same after reloading.
    Implemented missing axis parameter for np.stack (added some handy tuple calls too like the np.concatenate example).

--- test/TensorFlowNET.Keras.UnitTest/Layers/Layers.Merging.Test.cs
+++ test/TensorFlowNET.Keras.UnitTest/Layers/Layers.Merging.Test.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Tensorflow.NumPy;
 using static Tensorflow.KerasApi;
 
@@ -8,12 +9,16 @@ namespace Tensorflow.Keras.UnitTest.Layers
     public class LayersMergingTest : EagerModeTestBase
     {
         [TestMethod]
-        public void Concatenate()
+        [DataRow(1, 4, 1, 5)]
+        [DataRow(2, 2, 2, 5)]
+        [DataRow(3, 2, 1, 10)]
+        public void Concatenate(int axis, int shapeA, int shapeB, int shapeC)
         {
-            var x = np.arange(20).reshape((2, 2, 5));
-            var y = np.arange(20, 30).reshape((2, 1, 5));
-            var z = keras.layers.Concatenate(axis: 1).Apply(new Tensors(x, y));
-            Assert.AreEqual((2, 3, 5), z.shape);
+            var x = np.arange(10).reshape((1, 2, 1, 5));
+            var y = np.arange(10, 20).reshape((1, 2, 1, 5));
+            var z = keras.layers.Concatenate(axis: axis).Apply(new Tensors(x, y));
+            Assert.AreEqual((1, shapeA, shapeB, shapeC), z.shape);
         }
+
     }
 }
