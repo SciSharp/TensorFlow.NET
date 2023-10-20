3fcc4d8d1540c7c01ce4ca05ea883874abd4e5e5
Wanglongzhi2001 <583087864@qq.com>
2023-10-20 11:30:33 +0800

    fix: add the GRU, LSTM, SimpleRNN's OptionalArgs

---
+++ src/TensorFlowNET.Core/Keras/ArgsDefinition/Rnn/LSTMOptionalArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensorflow.Keras.ArgsDefinition.Rnn
+{
+    public class LSTMOptionalArgs : RnnOptionalArgs
+    {
+        public string Identifier => "LSTM";
+    }
+}
